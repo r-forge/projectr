6924aba6dd9b83dddd509bb8da58665180ef3560
richierocks (Mr ) <richierocks@gmail.com>
2011-05-10 22:15:28 +0000

    Functions that probably don't need to be called by the user.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/projectr@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/projectr/R/backend.r
@@ -0,0 +1,63 @@
+path_expand <- function(path, expand)
+{
+  #Converts a relative path to an absolute path in the PWD
+  if(is.null(path)) warning("path is NULL")
+  if(expand) path <- file.path(getwd(), path)
+  path
+}
+
+is_data_suitable_for_projects_option <- function(data)
+{
+  #Checks to see if the input is suitable to be used for getOption("projects")
+  if(!is.data.frame(data))
+  {
+    warning("The input is not a data.frame")
+    return(FALSE)
+  }    
+  if(nrow(data) == 0L)
+  {
+    warning("There are no rows in the data frame")
+    return(FALSE)
+  }
+  required_cols <- c("name", "path")
+  for(column in required_cols)
+  {
+    if(!(column %in% colnames(data)))
+    {
+      warning(paste("There is no column named", sQuote(column)))
+      return(FALSE)
+    }
+    if(!is.character(data[[column]]))
+    {
+      warning(paste("The", sQuote(column), "column does not have mode 'character'"))
+      return(FALSE)
+    }
+  } 
+  if(anyDuplicated(data$name))
+  {
+    warning("The values in the name column are not all unique")
+    return(FALSE)
+  }
+  rx <- '[*?"<>|]'
+  if(.Platform$OS.type == "windows" && any(grepl(rx, data$path)))
+  {
+    warning("The path column contains characters that are forbidden in Windows paths")
+    return(FALSE)
+  }  
+  TRUE
+}
+
+is_data_suitable_for_project_dirs_option <- function(data)
+{          
+  #Checks to see if the input is suitable to be used for getOption("project_dirs")
+  if(!is_data_suitable_for_projects_option(data))
+  {
+    return(FALSE)
+  }
+  if(!all(is_valid_variable_name(data$name)))
+  {
+    warning("The values in the name column are not all unique valid variable names")
+    return(FALSE)
+  }
+  return(TRUE)
+}
